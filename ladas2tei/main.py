df9e2663eb16926b1464239458020021094d7da3
Juliettejns <juliette.janes@yahoo.fr>
2024-11-29 17:11:50 +0100

    modif mineure bug

--- ladas2tei/main.py
+++ ladas2tei/main.py
@@ -234,7 +234,7 @@ def main(csv_metadata, pattern_header):
             block_str = "".join(liste_block)
             block_tei = ET.fromstring(block_str)
             root_xml.append(block_tei)
-            output=os.path/basename(row["file_name"])
+            output=os.path.basename(row["file_name"])
             with open(f'TEI/{output}.xml', "w") as f:
                 f.write(ET.tostring(root_xml, encoding='unicode', pretty_print=True))
         
